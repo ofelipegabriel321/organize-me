5ac8a9df4f1f2389d00bd4480bd59012dfc03836
Felipe Gabriel <ofelipegabriel321@gmail.com>
2020-09-21 15:37:11 -0300

    feat: add form dialog in categories screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,3 +1,4 @@
+import 'package:OrganizeMe/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatefulWidget {
@@ -6,8 +7,71 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  _showFormDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (param) {
+        return AlertDialog(
+          actions: <Widget>[
+            FlatButton(
+              color: Colors.red,
+              onPressed: () => Navigator.pop(context),
+              child: Text('Cancel'),
+            ),
+            FlatButton(
+              color: Colors.blue,
+              onPressed: () {},
+              child: Text('Save'),
+            ),
+          ],
+          title: Text('Categories Form'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Write a category',
+                    labelText: 'Category',
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Write a description',
+                    labelText: 'Description',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
-  Widget build (BuildContext context) {
-    return Text('Welcome to Categories Screen!');
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: RaisedButton(
+          onPressed: () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => HomeScreen())),
+          elevation: 0.0,
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          color: Colors.blue,
+        ),
+        title: Text('Categories'),
+      ),
+      body: Center(child: Text('Welcome to Categories Screen')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showFormDialog(context);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
